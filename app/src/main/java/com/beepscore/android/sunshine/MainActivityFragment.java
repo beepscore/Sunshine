4100ab8912eb693d5fb0bdff234547232b0109be
beepscore <support@beepscore.com>
2015-06-15 19:40:14 -0700

    In fragment_main.xml add dateView. In MainActivityFragment.java add configureDateViewInView.

--- app/src/main/java/com/beepscore/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/beepscore/android/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -18,6 +23,25 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View fragmentMainView = inflater.inflate(R.layout.fragment_main, container, false);
+        configureDateViewInView(fragmentMainView);
+        return fragmentMainView;
+    }
+
+    private void configureDateViewInView(View fragmentMainView) {
+        // Android TextClock would be nice, but requires SDK >= 17. So don't use it.
+        // https://developer.android.com/reference/android/widget/TextClock.html
+
+        TextView dateView = (TextView) fragmentMainView.findViewById(R.id.dateView);
+
+        // use user's preselected settings
+        DateFormat dateFormat = DateFormat.getDateInstance();
+
+        // Get calendar with default locale and time zone. Don't assume Gregorian
+        Calendar calendar = Calendar.getInstance();
+        Date now = calendar.getTime();
+
+        String formattedDate = dateFormat.format(now);
+        dateView.setText(formattedDate);
     }
 }
