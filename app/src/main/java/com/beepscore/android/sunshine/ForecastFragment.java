b063753bfbc04a61b7af116bb7007abe10ef5cdd
beepscore <support@beepscore.com>
2015-06-21 18:18:30 -0700

    In ForecastFragment add class FetchWeatherTask extends AsyncTask.
    In method body, use code from Udacity supplied snippet
    https://gist.github.com/anonymous/1c04bf2423579e9d2dcd/download#
    
    In onCreateView call fetchWeatherTask.execute()
    Initially app crashed, fixed by adding internet permission in AndroidManifest.xml.

--- app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
@@ -1,7 +1,9 @@
 package com.beepscore.android.sunshine;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +46,11 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView)fragmentForecastView.findViewById(R.id.listview_forecast);
         listView.setAdapter(adapter);
 
+        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+        //String urlString  = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+        String urlString  = "http://api.geonames.org/findNearByWeatherJSON?formatted=true&lat=42&lng=-2&username=demo&style=full";
+        fetchWeatherTask.execute(urlString);
+
         return fragmentForecastView;
     }
 
@@ -48,6 +61,96 @@ public class ForecastFragment extends Fragment {
         weekForecast.add("Thurs - Rainy - 64/51");
         weekForecast.add("Fri - Foggy - 70/46");
         weekForecast.add("Sat - Sunny - 76/68");
+    }
+
+    // https://developer.android.com/guide/components/processes-and-threads.html#Threads
+    // http://stackoverflow.com/questions/9671546/asynctask-android-example?rq=1
+    private class FetchWeatherTask extends AsyncTask<String, Void, String> {
+
+        /** The system calls this on a worker (background) thread
+         *  @param params first and only element is url string to make request to web service
+         *  @return json response from web service
+         */
+        @Override
+        protected String doInBackground(String... params) {
+
+            String urlString = params[0];
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL(urlString);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+
+                // If call connect on main thread app will crash
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("ForecastFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                forecastJsonStr = null;
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("ForecastFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return forecastJsonStr;
+        }
+
+        /** Use to update UI. The system calls this on the UI thread.
+         *  @param forecastJsonStr is the result returned from doInBackground()
+         */
+        @Override
+        protected void onPostExecute(String forecastJsonStr) {
+            super.onPostExecute(forecastJsonStr);
+            // TODO: parse forecastJsonStr in background? Show results in fragment view
+            Log.d("ForecastFragment", forecastJsonStr);
+        }
+
     }
 
 }
