41bc7c19fcac6f4ba4626ebd105c20a653aa8d53
beepscore <support@beepscore.com>
2015-06-22 21:33:56 -0700

    Add WeatherHelper and WeatherHelperTest.

---
+++ app/src/androidTest/java/com/beepscore/android/sunshine/WeatherHelperTest.java
@@ -0,0 +1,22 @@
+package com.beepscore.android.sunshine;
+
+import android.app.Application;
+import android.net.Uri;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class WeatherHelperTest extends ApplicationTestCase<Application> {
+    public WeatherHelperTest() {
+        super(Application.class);
+    }
+
+    public void testWeatherUri() {
+        Uri expected = Uri.parse("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+        Uri actual = WeatherHelper.weatherUri("94043");
+        assertEquals(expected, actual);
+    }
+
+}
+
---
+++ app/src/main/java/com/beepscore/android/sunshine/WeatherHelper.java
@@ -0,0 +1,29 @@
+package com.beepscore.android.sunshine;
+
+import android.net.Uri;
+
+/**
+ * Created by stevebaker on 6/22/15.
+ */
+public class WeatherHelper {
+
+    protected static Uri weatherUri(String postalCode) {
+
+        // http://stackoverflow.com/questions/19167954/use-uri-builder-in-android-or-create-url-with-variables
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http")
+                .authority("api.openweathermap.org")
+                .appendPath("data")
+                .appendPath("2.5")
+                .appendPath("forecast")
+                .appendPath("daily")
+                .appendQueryParameter("q", postalCode)
+                .appendQueryParameter("mode", "json")
+                .appendQueryParameter("units", "metric")
+                .appendQueryParameter("cnt", "7");
+
+        Uri uri = builder.build();
+        return uri;
+    }
+
+}
