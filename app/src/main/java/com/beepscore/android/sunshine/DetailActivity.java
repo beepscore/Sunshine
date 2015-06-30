220c95823a45fd517b409949a328e0af0c058d6a
beepscore <support@beepscore.com>
2015-06-30 11:57:43 -0700

    Rename menu_detail.xml to detail.xml as suggested in Lesson 3 video "Match Our Code"

--- app/src/main/java/com/beepscore/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/beepscore/android/sunshine/DetailActivity.java
@@ -18,7 +18,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
 
