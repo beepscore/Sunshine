a163c972d04f4f985ed14a61082c064bb15c05dd
beepscore <support@beepscore.com>
2015-07-03 21:28:28 -0700

    Add ShareActionProvider to fragment inside DetailActivity, as shown in Lesson 3 video Share Intent Solution.
    I was getting error like ShareActionProvider couldn't be cast to ActionProvider.
    Answer showed how to avoid this by using
        (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem)
    
    Rename detail.xml to detailfragment.xml, add action_share.
    Renamed PlaceholderFragment to DetailFragment.

--- app/src/main/java/com/beepscore/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/beepscore/android/sunshine/DetailActivity.java
@@ -3,15 +3,20 @@ package com.beepscore.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+// References:
 // https://github.com/udacity/Sunshine-Version-2/blob/3.02_create_detail_activity/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
 
 public class DetailActivity extends AppCompatActivity {
@@ -22,7 +27,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
         // http://www.androidhive.info/2013/11/android-working-with-action-bar
@@ -34,11 +39,9 @@ public class DetailActivity extends AppCompatActivity {
         actionBar.setIcon(R.drawable.ic_launcher);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
 
@@ -49,36 +52,90 @@ public class DetailActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    ////////////////////////////////////////////////////////////////////////////
+    public static class DetailFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+        private ShareActionProvider mShareActionProvider;
+        private String dayForecast = "";
+
+        public DetailFragment() {
+            // setHasOptionsMenu to ensure options menu methods get called
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            // http://stackoverflow.com/questions/11387740/where-how-to-getintent-getextras-in-an-android-fragment
-            String dayForecast = getActivity().getIntent().getExtras().getString(Intent.EXTRA_TEXT);
-
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            TextView textView = (TextView)rootView.findViewById(R.id.detail_text_view);
-            textView.setText(dayForecast);
+
+            // get the intent the activity was started with
+            // http://stackoverflow.com/questions/11387740/where-how-to-getintent-getextras-in-an-android-fragment
+            Intent intent = getActivity().getIntent();
+
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                dayForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                TextView textView = (TextView)rootView.findViewById(R.id.detail_text_view);
+                textView.setText(dayForecast);
+            }
 
             return rootView;
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            // http://stackoverflow.com/questions/15653737/oncreateoptionsmenu-inside-fragments
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            MenuItem shareMenuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+
+            // Attach an intent to this ShareActionProvider.
+            // You can update this at any time, such as when
+            // the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null.");
+            }
+
+            super.onCreateOptionsMenu(menu, inflater);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+
+            if (id == R.id.action_settings) {
+                Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            }
+
+            if (id == R.id.action_share) {
+                // do nothing. mShareActionProvider will handle click.
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            // TODO: Consider replace deprecated FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, dayForecast + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
     }
 }
