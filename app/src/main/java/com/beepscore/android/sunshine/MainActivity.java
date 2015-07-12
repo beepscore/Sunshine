33dbb77b102d906f0420973120adc95037bb4e25
beepscore <support@beepscore.com>
2015-07-12 16:31:30 -0700

    In MainActivity log lifecycle event callbacks.
    Reference:
    Udacity Android Lesson 4A: Activity lifecycle and data / lifecycle events.
    After app has started, rotate device.
    App logs destruction and recreation of activity in order below.
    onPause
    onStop
    onDestroy
    onCreate
    onStart
    onResume

--- app/src/main/java/com/beepscore/android/sunshine/MainActivity.java
+++ app/src/main/java/com/beepscore/android/sunshine/MainActivity.java
@@ -4,17 +4,55 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    ///////////////////////////////////////////////////////////////////////////
+    // activity lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "onCreate");
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, " onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(LOG_TAG, " onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(LOG_TAG, " onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LOG_TAG, " onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, " onDestroy");
+        super.onDestroy();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
