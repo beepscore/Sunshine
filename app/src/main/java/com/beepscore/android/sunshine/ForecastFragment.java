cb5b6e43b8452e6432f21ee21fca7847ca504c31
beepscore <support@beepscore.com>
2015-07-25 12:55:44 -0700

    In ForecastFragment delete inner class FetchWeatherTask and instead use "database friendly" FetchWeatherTask.java
    Also delete method temperatureInPreferredUnits and unused imports.

--- app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
@@ -1,11 +1,8 @@
 package com.beepscore.android.sunshine;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,17 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.json.JSONException;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 
@@ -115,183 +102,9 @@ public class ForecastFragment extends Fragment {
     }
 
     private void fetchWeatherForLocationPreference() {
-        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask(getActivity(), adapter);
         String locationPreferenceString = PreferenceHelper.getLocationPreferenceString(getActivity());
         fetchWeatherTask.execute(locationPreferenceString);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////
-
-    // https://developer.android.com/guide/components/processes-and-threads.html#Threads
-    // Nested classes
-    // http://docs.oracle.com/javase/tutorial/java/javaOO/nested.html
-    // http://stackoverflow.com/questions/9671546/asynctask-android-example?rq=1
-    // first parameter is doInBackground first argument params[0].
-    // second is onProgressUpdate integer argument.
-    // third is doInBackground return type and onPostExecute argument type.
-    private class FetchWeatherTask extends AsyncTask<String, Void, ArrayList<HashMap<String, String>>> {
-
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-        /** The system calls this on a worker (background) thread
-         *  @param params first and only element is postcode string to make request to web service
-         *  @return json response from web service. return null if no input.
-         */
-        @Override
-        protected ArrayList<HashMap<String, String>> doInBackground(String... params) {
-
-            String postcode = params[0];
-            final Integer NUM_DAYS = 7;
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            try {
-
-                Uri weatherUri = WeatherHelper.weatherJsonMetricUri(postcode, NUM_DAYS);
-                URL weatherUrl = new URL(weatherUri.toString());
-
-                urlConnection = (HttpURLConnection) weatherUrl.openConnection();
-                urlConnection.setRequestMethod("GET");
-
-                // If call connect on main thread app will crash
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
-                return null;
-
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                WeatherDataParser weatherDataParser = new WeatherDataParser();
-                ArrayList<HashMap<String, String>> forecastResults = weatherDataParser.getWeatherDataFromJson(forecastJsonStr, NUM_DAYS);
-                return forecastResults;
-            } catch (JSONException e) {
-                // Couldn't parse json
-                Log.e(LOG_TAG, "Error ", e);
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        /** Use to update UI. The system calls this on the UI thread.
-         *  @param forecastResults is the result returned from doInBackground()
-         */
-        @Override
-        protected void onPostExecute(ArrayList<HashMap<String, String>> forecastResults) {
-            super.onPostExecute(forecastResults);
-            if (forecastResults != null) {
-
-                ArrayList<String> forecastStrings = new ArrayList<String>();
-
-                for (int index = 0; index < forecastResults.size(); ++index) {
-                    HashMap<String, String> dayMap = forecastResults.get(index);
-
-                    String forecastString = "";
-                    if (dayMap != null) {
-
-                        if (dayMap.containsKey(WeatherDataParser.DAY)
-                                && (dayMap.get(WeatherDataParser.DAY) != null)) {
-                            String descriptionString = dayMap.get(WeatherDataParser.DAY);
-                            forecastString = forecastString + descriptionString;
-                        }
-
-                        forecastString = forecastString + " ";
-
-                        if (dayMap.containsKey(WeatherDataParser.DESCRIPTION)
-                                && (dayMap.get(WeatherDataParser.DESCRIPTION) != null)) {
-                            String descriptionString = dayMap.get(WeatherDataParser.DESCRIPTION);
-                            forecastString = forecastString + descriptionString;
-                        }
-
-                        forecastString = forecastString + " ";
-
-                        if (dayMap.containsKey(WeatherDataParser.OWM_MAX)
-                                && (dayMap.get(WeatherDataParser.OWM_MAX) != null)) {
-                            String maxString = dayMap.get(WeatherDataParser.OWM_MAX);
-                            String temperatureInPreferredUnits = temperatureInPreferredUnits(maxString);
-                            forecastString = forecastString + temperatureInPreferredUnits;
-                        }
-
-                        forecastString = forecastString + " / ";
-
-                        if (dayMap.containsKey(WeatherDataParser.OWM_MIN)
-                                && (dayMap.get(WeatherDataParser.OWM_MIN) != null)) {
-                            String minString = dayMap.get(WeatherDataParser.OWM_MIN);
-                            String temperatureInPreferredUnits = temperatureInPreferredUnits(minString);
-                            forecastString = forecastString + temperatureInPreferredUnits;
-                        }
-
-                        forecastString = forecastString + " humidity: ";
-
-                        if (dayMap.containsKey(WeatherDataParser.OWM_HUMIDITY)
-                                && (dayMap.get(WeatherDataParser.OWM_HUMIDITY) != null)) {
-                            String humidityString = dayMap.get(WeatherDataParser.OWM_HUMIDITY);
-                            forecastString = forecastString + humidityString;
-                        }
-
-                    }
-                    forecastStrings.add(forecastString);
-                }
-                updateAdapter(adapter, forecastStrings);
-            }
-        }
-
-    }
-
-    private String temperatureInPreferredUnits(String temperatureDegreesCString) {
-        double temperatureDegreesC = Double.parseDouble(temperatureDegreesCString);
-        // getIntegerInstance rounds, doesn't truncate.
-        NumberFormat formatter = NumberFormat.getIntegerInstance();
-
-        if (PreferenceHelper.getUnitsPreferenceString(getActivity()) == getString(R.string.pref_units_imperial)) {
-            return formatter.format(WeatherHelper.degreesCToDegreesF(temperatureDegreesC))
-                    + getString(R.string.degreesF);
-        } else {
-            return formatter.format(temperatureDegreesC) + getString(R.string.degreesC);
-        }
     }
 
 }
