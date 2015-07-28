485ea5cb58ffdabd97bc349533bb74a33c405276
beepscore <support@beepscore.com>
2015-07-27 19:44:57 -0700

    Change code as described in instructions Lesson4c Hooking it up with loaders- using CursorAdapter.
    In ForecastFragment change from ArrayAdapter to ForecastAdapter.
    In FetchWeatherTask delete any code related to updating adapter. Delete code for updating UI, it was moved to ForecastAdapter.

--- app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
@@ -1,6 +1,8 @@
 package com.beepscore.android.sunshine;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,13 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.beepscore.android.sunshine.Utility;
+import com.beepscore.android.sunshine.data.WeatherContract;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,7 +25,7 @@ public class ForecastFragment extends Fragment {
     public ForecastFragment() {
     }
 
-    ArrayAdapter<String> adapter = null;
+    ForecastAdapter mForecastAdapter = null;
 
     @Override
     // onCreate is called before onCreateView
@@ -38,28 +37,24 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String locationSetting = Utility.getPreferredLocation(getActivity());
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                locationSetting, System.currentTimeMillis());
+
+        Cursor cur = getActivity().getContentResolver().query(weatherForLocationUri,
+                null, null, null, sortOrder);
+
+        // The CursorAdapter will take data from our cursor and populate the ListView
+        // However, we cannot use FLAG_AUTO_REQUERY since it is deprecated, so we will end
+        // up with an empty list the first time we run.
+        mForecastAdapter = new ForecastAdapter(getActivity(), cur, 0);
+
         View fragmentForecastView = inflater.inflate(R.layout.fragment_forecast, container, false);
-
-        List<String> weekForecast = new ArrayList<String>();
-
-        // adapter creates views for each list item
-        adapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.list_item_forecast,
-                R.id.list_item_forecast_textview,
-                weekForecast);
-
         ListView listView = (ListView)fragmentForecastView.findViewById(R.id.listview_forecast);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String dayForecast = adapter.getItem(i);
-                Intent intent = new Intent(getActivity(), DetailActivity.class);
-                intent.putExtra(Intent.EXTRA_TEXT, dayForecast);
-                startActivity(intent);
-            }
-        });
+        listView.setAdapter(mForecastAdapter);
 
         return fragmentForecastView;
     }
@@ -67,15 +62,14 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        fetchWeatherForLocationPreference();
+        updateWeather();
     }
 
-    /*
-    private void updateAdapter(ArrayAdapter adapter, ArrayList<String> forecastStrings) {
-        adapter.clear();
-        adapter.addAll(forecastStrings);
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+        String location = Utility.getPreferredLocation(getActivity());
+        weatherTask.execute(location);
     }
-    */
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -90,7 +84,7 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
 
         if (id == R.id.action_refresh) {
-            fetchWeatherForLocationPreference();
+            updateWeather();
             return true;
         }
 
@@ -101,12 +95,6 @@ public class ForecastFragment extends Fragment {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void fetchWeatherForLocationPreference() {
-        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask(getActivity(), adapter);
-        String locationPreferenceString = PreferenceHelper.getLocationPreferenceString(getActivity());
-        fetchWeatherTask.execute(locationPreferenceString);
     }
 
 }
