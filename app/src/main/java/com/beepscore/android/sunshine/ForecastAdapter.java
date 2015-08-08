dd68cf2f6359f7b3f53eb5431a9d236b2129f62d
beepscore <support@beepscore.com>
2015-08-07 21:24:50 -0700

    feat: Change list_item_forecast.xml to show multiple views including image.
    Reference
    Lesson 5 Rich and responsive layouts
    Building list item

--- app/src/main/java/com/beepscore/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/beepscore/android/sunshine/ForecastAdapter.java
@@ -6,6 +6,9 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.beepscore.android.sunshine.data.WeatherContract;
@@ -15,6 +18,11 @@ import com.beepscore.android.sunshine.data.WeatherContract;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private String weatherDay = "";
+    private String weatherDesc = "";
+    private double weatherTemperatureMax;
+    private double weatherTemperatureMin;
 
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -62,7 +70,66 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        updateWeatherProperties(cursor);
+
+        LinearLayout list_item_layout = (LinearLayout)view;
+
+        ImageView imageView = (ImageView)list_item_layout.findViewById(R.id.list_item_forecast_imageview);
+        imageView.setImageResource(imageResourceForWeatherDescription(weatherDesc));
+
+        RelativeLayout dayAndDescription = (RelativeLayout)list_item_layout.findViewById(R.id.list_item_forecast_day_description);
+
+        TextView dayView = (TextView)dayAndDescription.findViewById(R.id.list_item_forecast_day);
+        dayView.setText(weatherDay);
+
+        TextView descriptionView = (TextView)dayAndDescription.findViewById(R.id.list_item_forecast_description);
+        descriptionView.setText(weatherDesc);
+
+        RelativeLayout temperaturesView = (RelativeLayout)list_item_layout.findViewById(R.id.list_item_temperatures);
+        TextView temperatureMaxTextView = (TextView)temperaturesView.findViewById(R.id.temperature_max_text_view);
+        String temperatureMax = Utility.formatTemperature(weatherTemperatureMax, true)
+                + "°";
+        temperatureMaxTextView.setText(temperatureMax);
+
+        TextView temperatureMinTextView = (TextView)temperaturesView.findViewById(R.id.temperature_min_text_view);
+        String temperatureMin = Utility.formatTemperature(weatherTemperatureMin, true)
+                + "°";
+        temperatureMinTextView.setText(temperatureMin);
+    }
+
+    private void updateWeatherProperties(Cursor cursor) {
+        weatherDay = Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE));
+        weatherDesc = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+        weatherTemperatureMax = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        weatherTemperatureMin = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+    }
+
+    private int imageResourceForWeatherDescription(String weatherDescription) {
+        int imageResource = -1;
+        switch (weatherDescription) {
+            case "Clear": {
+                imageResource = R.drawable.ic_clear;
+                break;
+            }
+            case "Clouds": {
+                imageResource = R.drawable.ic_cloudy;
+                break;
+            }
+            case "Extreme": {
+                imageResource = R.drawable.ic_storm;
+                break;
+            }
+            case "Rain": {
+                imageResource = R.drawable.ic_rain;
+                break;
+            }
+            case "Snow": {
+                imageResource = R.drawable.ic_snow;
+                break;
+            }
+            default:
+                imageResource = R.drawable.ic_launcher;
+        }
+        return imageResource;
     }
 }
