beepscore/Sunshine

commit 56ab8dc057541ad2b475c7a899965cf61cb1d736

feat: If weather list is empty, show empty view with text "No Weather Information Available".
Fix 2 crashes that could happen if location is empty- one in WeatherProvider and one in SyncAdapter.

app/src/main/java/com/beepscore/android/sunshine/data/WeatherProvider.java
@@ -197,15 +197,20 @@ public class WeatherProvider extends ContentProvider {
             }
             // "weather"
             case WEATHER: {
-                retCursor = mOpenHelper.getReadableDatabase().query(
-                        WeatherContract.WeatherEntry.TABLE_NAME,
-                        projection,
-                        selection,
-                        selectionArgs,
-                        null,
-                        null,
-                        sortOrder
-                );
+                if ((selection == null)
+                        && (selectionArgs == null)) {
+                    retCursor = null;
+                } else {
+                    retCursor = mOpenHelper.getReadableDatabase().query(
+                            WeatherContract.WeatherEntry.TABLE_NAME,
+                            projection,
+                            selection,
+                            selectionArgs,
+                            null,
+                            null,
+                            sortOrder
+                    );
+                }
                 break;
             }
             // "location"
@@ -225,8 +230,10 @@ public class WeatherProvider extends ContentProvider {
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
-        // set cursor to observe content provider for any changes in uri or descendants
-        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        if (retCursor != null) {
+            // set cursor to observe content provider for any changes in uri or descendants
+            retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        }
         return retCursor;
     }
 

app/src/main/java/com/beepscore/android/sunshine/ForecastFragment.java
@@ -91,6 +91,7 @@ public class ForecastFragment extends Fragment
     private final static int LOADER_ID = 1;
     private ForecastAdapter mForecastAdapter = null;
     private ListView mListView = null;
+    private View mEmptyView;
     private String dayForecast = "";
 
     static final String SELECTED_KEY = "POSITION";
@@ -132,6 +133,9 @@ public class ForecastFragment extends Fragment
         ListView listView = (ListView)fragmentForecastView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
         mListView = listView;
+
+        mEmptyView = fragmentForecastView.findViewById(R.id.empty_view);
+        mListView.setEmptyView(mEmptyView);
 
         if ((savedInstanceState != null)
                 && savedInstanceState.containsKey(SELECTED_KEY)) {

app/src/main/java/com/beepscore/android/sunshine/sync/SunshineSyncAdapter.java
@@ -225,6 +225,10 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 return;
             }
             forecastJsonStr = buffer.toString();
+            if (forecastJsonStr.contains("Error: Not found city")) {
+                Log.e(LOG_TAG, "Error: Not found city");
+                return;
+            }
             getWeatherDataFromJson(forecastJsonStr, locationQuery);
         } catch (IOException e) {
             Log.e(LOG_TAG, "Error ", e);

app/src/main/res/layout/fragment_forecast.xml
@@ -9,4 +9,10 @@
         android:layout_height="match_parent"
         style="@style/ForecastListStyle"
         android:id="@+id/listview_forecast"/>
+
+    <TextView android:id="@+id/empty_view"
+              android:layout_width="match_parent"
+              android:layout_height="match_parent"
+              android:text="@string/no_weather_info_available"/>
+
 </FrameLayout>

app/src/main/res/values/strings.xml
@@ -61,4 +61,5 @@
     <string name="enable_notifications" translatable="false">weather_notification</string>
     <string name="pref_weather_notification_title">Notification</string>
 
+    <string name="no_weather_info_available">No Weather Information Available</string>
 </resources>

commit 84220ed8152d063e71fd6d164efaa4fe4d6afbe8

fix: Partially undo commit 56ab8dc which accidentally broke WeatherProvider unit tests.

app/src/main/java/com/beepscore/android/sunshine/data/WeatherProvider.java
@@ -197,20 +197,15 @@ public class WeatherProvider extends ContentProvider {
             }
             // "weather"
             case WEATHER: {
-                if ((selection == null)
-                        && (selectionArgs == null)) {
-                    retCursor = null;
-                } else {
-                    retCursor = mOpenHelper.getReadableDatabase().query(
-                            WeatherContract.WeatherEntry.TABLE_NAME,
-                            projection,
-                            selection,
-                            selectionArgs,
-                            null,
-                            null,
-                            sortOrder
-                    );
-                }
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
                 break;
             }
             // "location"