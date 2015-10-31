db475b8a117214df866d7f7c0293dea6a2088f54
beepscore <support@beepscore.com>
2015-10-30 21:15:11 -0700

    test: Add LocationStatusUtilsTest

---
+++ app/src/androidTest/java/com/beepscore/android/sunshine/LocationStatusUtilsTest.java
@@ -0,0 +1,35 @@
+package com.beepscore.android.sunshine;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+/**
+ * Created by stevebaker on 10/30/15.
+ */
+public class LocationStatusUtilsTest extends ApplicationTestCase<Application> {
+    public LocationStatusUtilsTest() {
+        super(Application.class);
+    }
+
+    public void testLocationStatus() {
+        int actual;
+        int fakeStatus;
+
+        fakeStatus = LocationStatusUtils.LOCATION_STATUS_OK;
+        LocationStatusUtils.setLocationStatus(getContext(), fakeStatus);
+        actual = LocationStatusUtils.getLocationStatus(getContext());
+        assertEquals(fakeStatus, actual);
+
+        fakeStatus = LocationStatusUtils.LOCATION_STATUS_SERVER_INVALID;
+        LocationStatusUtils.setLocationStatus(getContext(), fakeStatus);
+        actual = LocationStatusUtils.getLocationStatus(getContext());
+        assertEquals(fakeStatus, actual);
+
+        // clean up
+        fakeStatus = LocationStatusUtils.LOCATION_STATUS_UNKNOWN;
+        LocationStatusUtils.setLocationStatus(getContext(), fakeStatus);
+        actual = LocationStatusUtils.getLocationStatus(getContext());
+        assertEquals(fakeStatus, actual);
+    }
+}
