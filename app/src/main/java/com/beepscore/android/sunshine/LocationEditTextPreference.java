935b6ff60bb2d6fc56edf3b029c49e7974111926
beepscore <support@beepscore.com>
2015-12-12 20:04:55 -0800

    Delete unused import Log. Note although video quiz says to log result, video solution just sets a breakpoint at LocationEditTextPreference a.recycle.
    Also reference repository does not log either.
    https://github.com/udacity/Advanced_Android_Development.git branch 2.08_Implement_Limits_Part_1

--- app/src/main/java/com/beepscore/android/sunshine/LocationEditTextPreference.java
+++ app/src/main/java/com/beepscore/android/sunshine/LocationEditTextPreference.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.preference.EditTextPreference;
 import android.util.AttributeSet;
-import android.util.Log;
 
 /**
  * LocationEditTextPreference is a type of view
