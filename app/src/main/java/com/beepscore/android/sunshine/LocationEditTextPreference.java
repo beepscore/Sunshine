56c9f6f1b181ebd9608e22eab3989522d245ba5e
beepscore <support@beepscore.com>
2015-12-12 20:42:15 -0800

    feat: Override showDialog to disable and enable OK button based on text length.
    I copied implementation from reference repository. Internet seems to indicate showDialog is deprecated, replaced by DialogFragments.
    So don't spend time learning showDialog().
    http://stackoverflow.com/questions/10285047/showdialog-deprecated-whats-the-alternative
    http://www.vogella.com/tutorials/AndroidDialogs/article.html

--- app/src/main/java/com/beepscore/android/sunshine/LocationEditTextPreference.java
+++ app/src/main/java/com/beepscore/android/sunshine/LocationEditTextPreference.java
@@ -1,9 +1,16 @@
 package com.beepscore.android.sunshine;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * LocationEditTextPreference is a type of view
@@ -34,6 +41,44 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        // super instantiates editText
+        super.showDialog(state);
+
+        // now getEditText will return non-null
+        EditText editText = getEditText();
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    // positiveButton is labeled "OK"
+                    Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    if (s.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+
+        });
     }
 
 }
