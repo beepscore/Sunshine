d62fa9978aba58503eb358fee14e346f9826d8a5
beepscore <support@beepscore.com>
2015-12-26 17:56:58 -0800

    In MyGcmListenerService revise comments

--- app/src/main/java/com/beepscore/android/sunshine/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/beepscore/android/sunshine/gcm/MyGcmListenerService.java
@@ -92,10 +92,13 @@ public class MyGcmListenerService extends GcmListenerService {
                 PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
 
         // Notifications using both a large and a small icon (which yours should!) need the large
-        // icon as a bitmap. So we need to create that here from the resource ID, and pass the
-        // object along in our notification builder. Generally, you want to use the app icon as the
-        // small icon, so that users understand what app is triggering this notification.
+        // icon as a bitmap.
+        // Create largeIcon from the resource ID
         Bitmap largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.art_storm);
+
+        // Generally, you want to set the small icon to the app icon,
+        // so that users understand what app is triggering this notification.
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.art_clear)
